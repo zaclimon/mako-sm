db9c4bafc4a54bcb1fd57ff0247fd174191b7aea
zaclimon <isaacpateau05@gmail.com>
2014-05-10 16:14:48 -0400

    Add verification for f2fs ramdisk

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-if [ $# -gt 0 ]; then
-echo $1 > .version
+
+RAMDISK="boot.img-ramdisk"
+
+if [[ ! "$1" =~ "f2fs" ]] ; then
+echo $RAMDISK
+fi
+ 
+if [[ "$1" =~ "f2fs" ]] ; then
+RAMDISK="boot.img-ramdisk-f2fs"
+echo $RAMDISK
 fi
  
 make -j16
@@ -9,7 +17,7 @@ cp arch/arm/boot/zImage ../ramdisk_mako/
 cd ../ramdisk_mako/
  
 echo "making ramdisk"
-./mkbootfs boot.img-ramdisk | gzip > ramdisk.gz
+./mkbootfs $RAMDISK | gzip > ramdisk.gz
 echo "making boot image"
 ./mkbootimg --kernel zImage --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31' --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x01600000 --ramdisk ramdisk.gz --output ../mako/boot.img
  
