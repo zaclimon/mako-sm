eaf695a2fb1182ee36e154d5024e8deae77f5731
zaclimon <isaacpateau05@gmail.com>
2014-07-31 19:53:09 -0400

    r210 is ready to go

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ JOBS=`grep -c "processor" /proc/cpuinfo`
 
 if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
 git checkout sm-4.4-sm
-zipfile="franco.Kernel-SaberMod-r209f-CM.zip"
+zipfile="franco.Kernel-SaberMod-r210-CM.zip"
 else
 git checkout sm-4.4
-zipfile="franco.Kernel-SaberMod-r209f.zip"
+zipfile="franco.Kernel-SaberMod-r210.zip"
 fi
 
 
